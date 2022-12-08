ea876687f02f38c47f3d96c94cb5fe655e07ddba
kunal.zanke <kunal.zanke@globallogic.com>
2022-12-08 13:06:35 +0530

    addNote fun

--- handlers/notes.ts
+++ handlers/notes.ts
@@ -1,30 +1,55 @@
 import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
+import { DynamoDBClient, BatchExecuteStatementCommand, PutItemCommand , } from "@aws-sdk/client-dynamodb";
+import { marshall } from"@aws-sdk/util-dynamodb";
+import { v4 as uuidv4 } from 'uuid';
+
+const TableName = "notes";
 
 export const addNote =  async (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback)  =>  {
 
-  console.log(event)
+  try {
+    
+      const dynamo = new DynamoDBClient({
+      region: "127.0.1.1",
+      endpoint: "http://127.0.1.1:8000"
+    });
+    
+    const { title , content , date, userId } = JSON.parse(event.body!);
+    const noteId = uuidv4();
+    const putCmd = new PutItemCommand({
+      TableName,
+      Item: marshall(
+        {
+        userId,
+        noteId,
+        title,
+        content,
+        date
+      }
+      )
+    });
   
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Adding Note....',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-}
-
-export async function getNote(event, context, callback) {
-
-  
+    const note = await dynamo.send(putCmd)
+    
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Getting Note....',
-        input: event,
+        message: 'Note Added Successfully',
+        note,
       }),
     };
-  
-    callback(null, response);
-  }
+    
+    return response;
+
+  } catch(e) {
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'General Server Error',
+        e,
+      }),
+    };
+    return response;
+  }    
+}
