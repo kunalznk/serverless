7f814d00439bd9ee450d4e91eeb49dd4e5739e27
kunal.zanke <kunal.zanke@globallogic.com>
2023-01-14 22:09:22 +0530

    integrate auth and marshall and unmarshall items

--- handlers/notes.ts
+++ handlers/notes.ts
@@ -1,15 +1,21 @@
 import { Context, APIGatewayProxyCallback, APIGatewayEvent } from 'aws-lambda';
 import { DeleteItemCommand, GetItemCommand, QueryCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { CreateTableCommand, DeleteItemCommand, GetItemCommand, QueryCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBClient, PutItemCommand , DynamoDBClientConfig} from "@aws-sdk/client-dynamodb";
-import { marshall } from"@aws-sdk/util-dynamodb";
+import { marshall , unmarshall} from"@aws-sdk/util-dynamodb";
 import { v4 as uuidv4 } from 'uuid';
+import jwtDecode from 'jwt-decode';
+import { CognitoIdentityClient, CreateIdentityPoolCommand , GetIdCommand , GetCredentialsForIdentityCommand , GetCredentialsForIdentityCommandOutput} from "@aws-sdk/client-cognito-identity";
 
-const TableName = "notes";
+const TableName = process.env.NOTES_TABLE;
 
 const clientOpt : DynamoDBClientConfig = {
   region: "127.0.1.1",
   endpoint: "http://127.0.1.1:8000"
 }
+
+const cognitoIdentity = new CognitoIdentityClient({ region: "region"});
+const identityPoolId = process.env.COGNITO_IDENTITY_POOL_ID;
 
 export const addNote =  async (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback)  =>  {
 
@@ -17,8 +23,8 @@ export const addNote =  async (event: APIGatewayEvent, context: Context, callbac
     
     const dynamo = new DynamoDBClient(clientOpt);
     
-    const { title , content , date, userId } = JSON.parse(event.body!);
-
+    const { title , description , date, time,  category } = JSON.parse(event.body!);
+    const userId = event.headers.app_user_id;
     const noteId = uuidv4();
     const putCmd = new PutItemCommand({
       TableName,
@@ -27,8 +33,10 @@ export const addNote =  async (event: APIGatewayEvent, context: Context, callbac
         userId,
         noteId,
         title,
-        content,
-        date
+        description,
+        date,
+        time,
+        category
       }
       )
     });
@@ -39,14 +47,13 @@ export const addNote =  async (event: APIGatewayEvent, context: Context, callbac
       statusCode: 200,
       body: JSON.stringify({
         message: 'Note Added Successfully',
-        note,
+        note: note.Attributes ? unmarshall(note.Attributes) : note,
       }),
     };
     
     return response;
 
   } catch(e) {
-    
     const response = {
       statusCode: 200,
       body: JSON.stringify({
@@ -63,25 +70,22 @@ export const getNotes =  async (event: APIGatewayEvent, context: Context, callba
   try {
     
       const dynamo = new DynamoDBClient(clientOpt);
-      
-      const user = "kunalznk-1";
-      
-      
+      const user = event.headers.app_user_id!;
       const getCmd = new QueryCommand({
         TableName,
           KeyConditionExpression: "userId = :user",
           ExpressionAttributeValues: {
             ":user" : { "S" : user }
-          }
+          },
       })
     
     const notes = await dynamo.send(getCmd);
-    
+
     const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Notes Fetched Successfully',
-        notes,
+        notes : notes.Items ? notes.Items.map((item) => unmarshall(item)) : notes
       }),
     };
     
@@ -107,7 +111,7 @@ export const getNoteById =  async (event: APIGatewayEvent, context: Context, cal
       const dynamo = new DynamoDBClient(clientOpt);
       const noteId = event.pathParameters?.noteId
       
-      const userId = "kunalznk-1";
+      const userId = event.headers.app_user_id!;
       
       const getCmd = new GetItemCommand({
         TableName,
@@ -118,12 +122,12 @@ export const getNoteById =  async (event: APIGatewayEvent, context: Context, cal
       })
     
     const note = await dynamo.send(getCmd);
-    
+
     const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: note?.Item ? 'Note Fetched Successfully' : 'Note not found',
-        note,
+        note: note.Item ? unmarshall(note.Item!) : note,
       }),
     };
     
@@ -147,11 +151,10 @@ export const updateNoteById =  async (event: APIGatewayEvent, context: Context, 
   try {
     
       const dynamo = new DynamoDBClient(clientOpt);
-      const {title , content , date } = JSON.parse(event.body!);
+      const {title , description , date } = JSON.parse(event.body!);
       const noteId = event.pathParameters?.noteId
       
-      const userId = "kunalznk-1";
-      // const noteId = "6a784ffb-de67-4cff-871d-b3d6a44855da"
+      const userId = event.headers.app_user_id!;
       const updateCmd = new UpdateItemCommand({
         TableName,
         Key: {
@@ -162,7 +165,7 @@ export const updateNoteById =  async (event: APIGatewayEvent, context: Context, 
             "S": userId
           }
         },
-        UpdateExpression: "set title = :t, content = :c, #date = :d",
+        UpdateExpression: "set title = :t, description = :c, #date = :d",
         ExpressionAttributeNames: {
           "#date": "date"
         },
@@ -171,7 +174,7 @@ export const updateNoteById =  async (event: APIGatewayEvent, context: Context, 
             "S": title
           },
           ":c" : {
-            "S": content
+            "S": description
           },
           ":d": {
             "S": date
@@ -181,12 +184,11 @@ export const updateNoteById =  async (event: APIGatewayEvent, context: Context, 
       })
     
     const notes = await dynamo.send(updateCmd);
-    console.log(notes)
     const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Note Updated Successfully',
-        notes,
+        notes : notes.Attributes ? unmarshall(notes.Attributes) : notes,
       }),
     };
     
@@ -212,7 +214,7 @@ export const deleteNoteById = async (event: APIGatewayEvent, context: Context, c
       const dynamo = new DynamoDBClient(clientOpt);
       const noteId = event.pathParameters?.noteId
       
-      const userId = "kunalznk-1";
+      const userId = event.headers.app_user_id!;
       const deleteCmd = new DeleteItemCommand({
         TableName,
         Key: {
@@ -238,6 +240,47 @@ export const deleteNoteById = async (event: APIGatewayEvent, context: Context, c
   } catch(e) {
     console.log(e)
     const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'General Server Error',
+        e,
+      }),
+    };
+    return response;
+  }    
+}
+
+export const authorize = async (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback)  =>  {
+
+  try {
+    
+    let id_token  = event.headers.Authorization;
+    let params = {
+      IdentityPoolId: identityPoolId,
+      Login:{ 'accounts.google.com' : id_token }
+    }
+    let data : any = await cognitoIdentity.send(new GetIdCommand(params));
+
+    data = await cognitoIdentity.send(new GetCredentialsForIdentityCommand({
+          IdentityId: data?.IdentityId,
+          Logins:  { 'accounts.google.com' : id_token! }
+    }))
+
+    let decoded : any = jwtDecode(id_token!);
+    data.userName = decoded.name
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+    
+    return response;
+
+  } catch(e) {
+    const response = {
+      headers : {
+        "Access-Control-Allow-Origin":  "*"
+      },
       statusCode: 200,
       body: JSON.stringify({
         message: 'General Server Error',
